fa4e412333540813102bf3b00e16321a035fdda7
nishant <nishanta454@gmail.com>
2023-08-02 12:33:06 +0530

    fist commit

---
+++ src/test/java/com/poc/xmltojson/XmlToJsonConverterTest.java
@@ -0,0 +1,21 @@
+package com.poc.xmltojson;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class XmlToJsonConverterTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
